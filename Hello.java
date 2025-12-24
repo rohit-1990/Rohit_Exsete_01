a7340ab917f7c64bf4f69839e558780fe514bb25
rohit-1990 <rs.rohit.sing@gmail.com>
2025-12-24 22:35:56 +0530

    this is my third commit messagejkkk updated 124

---
+++ Hello.java
@@ -0,0 +1,10 @@
+import java.sql.SQLOutput;
+
+public class Hello {
+    public static void main(String args[]){
+        System.out.println("hi ");
+        System.out.println("hi 1177");
+        System.out.println("yyyyy");
+
+    }
+}
